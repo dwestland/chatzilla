d765b9bd0c8154f6b5c909dd67f3ca95b267586f
dwestland <don@westland.net>
2023-08-02 13:39:11 -0700

    Update Pricing page

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -19,82 +19,117 @@ const Pricing  = () => {
         </p>
       </div>
 
-      <div className="my-8">
+      <div className="my-8 text-center">
         <div className="grid max-w-md gap-10 row-gap-5 lg:max-w-screen-lg sm:row-gap-10 lg:grid-cols-3 xl:max-w-screen-lg sm:mx-auto">
-          <div className="flex flex-col justify-between p-8 transition-shadow duration-300 bg-white border rounded-lg shadow-sm sm:items-center hover:shadow">
-            <div className="text-center">
-              <div className="text-lg font-semibold">Start</div>
-              <div className="flex items-center justify-center mt-2">
-                <div className="mr-1 text-5xl font-bold">Free</div>
-              </div>
-              <div className="mt-2 space-y-3">
-                <div className="text-gray-700">10 deploys per day</div>
-                <div className="text-gray-700">10 GB of storage</div>
-                <div className="text-gray-700">20 domains</div>
-              </div>
+
+          <div className="flex flex-col justify-between py-8 transition-shadow duration-300 dark:bg-gray-800 border-gray-200 dark:border-gray-700 border-2 rounded-lg shadow-sm sm:items-center hover:shadow">
+            <div className="text-lg font-semibold">Starter</div>
+            <div className="flex items-center justify-center mt-2">
+              <div className="mr-1 text-5xl font-bold">$24</div>
+              <div className="self-end">/ mo</div>
             </div>
-            <div>
-              <a
-                href="/"
-                className="inline-flex items-center justify-center w-full h-12 px-6 mt-6 font-medium tracking-wide text-white transition duration-200 bg-gray-800 rounded shadow-md hover:bg-gray-900 focus:shadow-outline focus:outline-none"
-              >
-                Start for free
-              </a>
-              <p className="max-w-xs mt-6 text-xs text-gray-600 sm:text-sm sm:text-center sm:max-w-sm sm:mx-auto">
-                Sed ut unde omnis iste natus accusantium doloremque.
-              </p>
+            <p className="text-sm mt-2">Paid annually, paid monthly $30 per month</p>
+
+            <div className="p-6">
+              <ul className="list-inside">
+                <li className="mb-4 flex">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
+                    stroke="#4f46e5" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Unlimited
+                  updates
+                </li>
+                <li className="mb-4 flex">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
+                    stroke="#4f46e5" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Git
+                  repository
+                </li>
+                <li className="mb-4 flex">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
+                    stroke="#4f46e5" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>npm
+                  installation
+                </li>
+              </ul>
             </div>
+
+            <Link href="/" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5">         
+              Get Started
+            </Link>
           </div>
-          <div className="relative flex flex-col justify-between p-8 transition-shadow duration-300 bg-white border rounded-lg shadow-sm sm:items-center hover:shadow border-deep-purple-accent-400">
-            <div className="text-center">
-              <div className="text-lg font-semibold">Pro</div>
-              <div className="flex items-center justify-center mt-2">
-                <div className="mr-1 text-5xl font-bold">$38</div>
-                <div className="text-gray-700">/ mo</div>
-              </div>
-              <div className="mt-2 space-y-3">
-                <div className="text-gray-700">200 deploys per day</div>
-                <div className="text-gray-700">80 GB of storage</div>
-                <div className="text-gray-700">Global CDN</div>
-              </div>
+
+          <div className="flex flex-col justify-between py-8 transition-shadow duration-300 dark:bg-gray-800 border-gray-200 dark:border-gray-700 border-2 rounded-lg shadow-sm sm:items-center hover:shadow">
+            <div className="text-lg font-semibold">Starter</div>
+            <div className="flex items-center justify-center mt-2">
+              <div className="mr-1 text-5xl font-bold">$24</div>
+              <div className="self-end">/ mo</div>
             </div>
-            <div>
-              <a
-                href="/"
-                className="inline-flex items-center justify-center w-full h-12 px-6 mt-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-              >
-                Buy Pro
-              </a>
-              <p className="max-w-xs mt-6 text-xs text-gray-600 sm:text-sm sm:text-center sm:max-w-sm sm:mx-auto">
-                Sed ut unde omnis iste natus accusantium doloremque.
-              </p>
+            <p className="text-sm mt-2">Paid annually, paid monthly $30 per month</p>
+
+            <div className="p-6">
+              <ul className="list-inside">
+                <li className="mb-4 flex">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
+                    stroke="#4f46e5" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Unlimited
+                  updates
+                </li>
+                <li className="mb-4 flex">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
+                    stroke="#4f46e5" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Git
+                  repository
+                </li>
+                <li className="mb-4 flex">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
+                    stroke="#4f46e5" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>npm
+                  installation
+                </li>
+              </ul>
             </div>
+
+            <Link href="/" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5">         
+              Get Started
+            </Link>
           </div>
-          <div className="flex flex-col justify-between p-8 transition-shadow duration-300 bg-white border rounded-lg shadow-sm sm:items-center hover:shadow">
-            <div className="text-center">
-              <div className="text-lg font-semibold">Business</div>
-              <div className="flex items-center justify-center mt-2">
-                <div className="mr-1 text-5xl font-bold">$78</div>
-                <div className="text-gray-700">/ mo</div>
-              </div>
-              <div className="mt-2 space-y-3">
-                <div className="text-gray-700">500 GB of storage</div>
-                <div className="text-gray-700">Unlimited domains</div>
-                <div className="text-gray-700">24/7 Support</div>
-              </div>
+
+          <div className="flex flex-col justify-between py-8 transition-shadow duration-300 dark:bg-gray-800 border-gray-200 dark:border-gray-700 border-2 rounded-lg shadow-sm sm:items-center hover:shadow">
+            <div className="text-lg font-semibold">Starter</div>
+            <div className="flex items-center justify-center mt-2">
+              <div className="mr-1 text-5xl font-bold">$24</div>
+              <div className="self-end">/ mo</div>
             </div>
-            <div>
-              <a
-                href="/"
-                className="inline-flex items-center justify-center w-full h-12 px-6 mt-6 font-medium tracking-wide text-white transition duration-200 bg-gray-800 rounded shadow-md hover:bg-gray-900 focus:shadow-outline focus:outline-none"
-              >
-                Buy Business
-              </a>
-              <p className="max-w-xs mt-6 text-xs text-gray-600 sm:text-sm sm:text-center sm:max-w-sm sm:mx-auto">
-                Sed ut unde omnis iste natus accusantium doloremque.
-              </p>
+            <p className="text-sm mt-2">Paid annually, paid monthly $30 per month</p>
+
+            <div className="p-6">
+              <ul className="list-inside">
+                <li className="mb-4 flex">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
+                    stroke="#4f46e5" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Unlimited
+                  updates
+                </li>
+                <li className="mb-4 flex">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
+                    stroke="#4f46e5" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Git
+                  repository
+                </li>
+                <li className="mb-4 flex">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
+                    stroke="#4f46e5" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>npm
+                  installation
+                </li>
+              </ul>
             </div>
+
+            <Link href="/" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5">         
+              Get Started
+            </Link>
           </div>
+
         </div>
       </div>
 
