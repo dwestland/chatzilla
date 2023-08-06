bc5fb39f4bb6579c441299e9cf2ac6f8355fe2b3
dwestland <don@westland.net>
2023-08-06 11:59:03 -0700

    Add OpenAI chat

---
+++ src/app/chatbot/page.tsx
@@ -0,0 +1,12 @@
+import Chat from "./chat";
+
+// export const runtime = 'edge'
+
+export default function Home() {
+  return (
+    <main className="container max-w-xl mx-auto">
+      <h1 className="text-center my-8 text-2xl">DonGPT</h1>
+      <Chat />
+    </main>
+  );
+}
